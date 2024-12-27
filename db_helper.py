ddd2c6fa16402064838ef22413b33faf5fc22b28
Thirniya-08 <thirniyap@gmail.com>
2024-12-28 00:06:35 +0530

    Update db_helper.py

--- db_helper.py
+++ db_helper.py
@@ -10,7 +10,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
-PORT = int(os.getenv("PORT"))
+PORT = 3306
 
 
 async def get_db_connection():
